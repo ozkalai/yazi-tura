641e20b7b4f8c99c33b5ff995b339d3450c42c73
ozkalai <melihozkalayy@gmail.com>
2021-06-05 14:43:00 +0300

    static number turn into dynamic and state updated

--- src/components/CoinFlipper/CoinFlipper.js
+++ src/components/CoinFlipper/CoinFlipper.js
@@ -10,13 +10,59 @@ class CoinFlipper extends Component {
     this.state = {
       side: "tura",
       flipping: false,
+      tossTime: 0,
+      random: 0,
+      yazi: 0,
+      tura: 0,
     };
+    this.randomNumber = this.randomNumber.bind(this);
+    this.currentSide = this.currentSide.bind(this);
+    this.numberOfSide = this.numberOfSide.bind(this);
   }
+  randomNumber = () => {
+    return Math.round(Math.random());
+  };
+
+  currentSide = () => {
+    if (this.state.random === 0) {
+      return "tura";
+    } else if (this.state.random === 1) {
+      return "yazi";
+    }
+  };
+
+  numberOfSide = () => {};
+
   handleClick = () => {
-    // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
-    this.setState({ flipping: true });
+    // "At" butonuna tıkladığımızda toplam atış sayısını tutmak için tossTime değerini 1 arttırıyoruz.
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        flipping: true,
+        tossTime: prevState.tossTime + 1,
+        random: this.randomNumber(),
+        side: this.currentSide(),
+      };
+    });
+    console.log(this.state.random);
+    console.log(this.state.side);
+    console.log(this.state.yazi);
+    console.log(this.state.tura);
     // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
     setTimeout(() => this.setState({ flipping: false }), 1000);
+    if (this.state.random === 0) {
+      this.setState((prevState) => {
+        return {
+          tura: prevState.tura + 1,
+        };
+      });
+    } else {
+      this.setState((prevState) => {
+        return {
+          yazi: prevState.yazi + 1,
+        };
+      });
+    }
   };
 
   render() {
@@ -27,10 +73,10 @@ class CoinFlipper extends Component {
         <button onClick={this.handleClick}>At!</button>
         <p>
           Toplam
-          <strong> 5 </strong>
+          <strong> {this.state.tossTime} </strong>
           atıştan
-          <strong> 3 </strong>ü tura
-          <strong> 2 </strong>
+          <strong> {this.state.tura} </strong>ü tura
+          <strong> {this.state.yazi} </strong>
           si yazı geldi.
         </p>
       </div>
